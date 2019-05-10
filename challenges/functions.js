91ae046fa3b6f08ac5ae1bb1a0b06413673a1db5
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-10 12:26:01 -0500

    functions,js almost completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,29 +6,51 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-     function consume(param1, param2, cb) {
-
-      function add cb(param1, param2) 
+    function consume(x, y, callback)  {
+      return  (callback,x, y);
+  
      }
 
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
 
+
+//  Step 2: Create several functions to callback with consume();
+//   * Create a function named add that returns the sum of two numbers
+
+       function add(x, y, callback) {
+          return callback(x + y);
+  
+  }
+
+          add(7,4, function(addition){
+            console.log(addition)
+});
+//   * Create a function named multiply that returns the product of two numbers 
+
+function multiply(x, y, callback) {
+  return callback(x * y);
+  
+  }
+
+multiply(7,4, function(multiply){
+ console.log(multiply)
+});
+//   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+// */
+function greeting (fn, ln) {
+  return  'Hello' + fn + ln, 'nice to meet you'
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+ consume(2,2,add); // 4
+ consume(10,16,multiply); // 160
+ consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: JavaScript functions can be nested within other functions. A nested (inner) function can access the arguments and variables of the (outer) function it is nested within.
 
 
 const external = "I'm outside the function";
